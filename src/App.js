6310bd4f3d7274b76ba537a5de9b1ed3ce4a491d
georgesndene <ndenegeorgesmichelngor@gmail.com>
2024-06-21 12:04:49 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Person: {
+        fullName: 'Georges NDENE',
+        bio: 'Un passionné du Developpement web du SENEGAL.',
+        imgSrc: 'IMG-16.jpg',
+        
+        profession: 'Développeur Web'
+      },
+      shows: false,
+      intervalId: null,
+      timeElapsed: 0
+    };
+  }
+
+  componentDidMount() {
+    const intervalId = setInterval(() => {
+      this.setState(prevState => ({
+        timeElapsed: prevState.timeElapsed + 1
+      }));
+    }, 1000);
+    this.setState({ intervalId });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  toggleShow = () => {
+    this.setState(prevState => ({
+      shows: !prevState.shows,
+      timeElapsed: 0
+    }));
+  };
+
+  render() {
+    const { Person, shows, timeElapsed } = this.state;
+
+    return (
+      <div className="App">
+        <button onClick={this.toggleShow}>
+          {shows ? 'Hide Profile' : 'Show Profile'}
+        </button>
+        {shows && (
+          <div>
+            <h1>{Person.fullName}</h1>
+            <p>{Person.bio}</p>
+            <img src={Person.imgSrc} alt="Profile" />
+            <p>{Person.profession}</p>
+          </div>
+        )}
+        <p>Time elapsed: {timeElapsed} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
